99ac7d0f2c14bab2c5b39aba2a477c46d2e192dc
Daniel Morgan <daniel.morgan@ext.ons.gov.uk>
2022-02-01 11:12:23 +0000

    Removed the functionality to calculate a rank instead of a quintile to present the spatial distribution of the features as a map on the dashboard. Now that ready meals and textiles features are summed, a quintile can be calculated. Improvements to comments and brevity of code.

--- src/pipeline/03_Output.py
+++ src/pipeline/03_Output.py
@@ -1,6 +1,3 @@
-## This script ingests the modelling results and transforms them to 
-
-# import modules
 import sys
 import os
 
@@ -30,12 +27,9 @@ df_pred_residuals = factory.get('tranche_residuals').create_dataframe()
 df_latest_pred = factory.get('tranche_latest_predictions').create_dataframe()
 
 # Model features
-df_features = factory.get('tranche_model_features').create_dataframe()
+df_features = factory.get('tranche_model_input').create_dataframe()
 
 ###### PROCESS ###########
-
-# TEST REQUIREMENT - MAKE SURE THAT ONLY OBJECT AND FLOAT32 DTYPES EXIST IN THE DATAFRAME BEFORE WRITING TO BIGQUERY
-# drop the 'Area' column - do this further back in the modelling process if possible
 
 # Subset for the columns required in the dashboard
 df_reg_coefs = df_reg_coefs[['Features','Coefficients','tranche', 'travel_cluster','Date']]
@@ -44,35 +38,22 @@ df_non_reg_non_std_coefs = df_non_reg_non_std_coefs[['Features','coef','P_value'
 df_pred_residuals = df_pred_residuals[['LSOA11CD','tranche','Residual']]
 df_latest_pred = df_latest_pred[['LSOA11CD','Predicted_cases_test','MSOA11NM']]
 
-# No temporal element is needed for plotting this data set
-df_features.drop('Date', axis=1, inplace=True)
-
 # Filter the features for the latest tranche for plotting
 df_features = df_features[df_features['tranche_order'] == cf.n_tranches]
 
 # Drop the column to allow for quintile calculation on all numerical columns
-df_features.drop(['tranche_order','tranche_desc','COVID_Cases_per_unit_area'], axis=1, inplace=True)
+df_features.drop(['Date','tranche_order','tranche_desc','COVID_Cases_per_unit_area'], axis=1, inplace=True)
 
 # compute quintiles for plotting
-# The ready_meals feature is dropped - too many zeroes to compute quintiles with unique bin edges. Rank is used instead
-df_quint = dash.make_quintiles(df_features.drop('ready_meals', axis=1))
-
-# Make a separate dataframe for just the ready_meals column
-df_ready_meals = df_features[['LSOA11CD', 'ready_meals']]
-
-# rank LSOAs by ready meals
-df_ready_meals['ready_meals_rank'] = df_ready_meals['ready_meals'].rank(method='max', ascending=False)
-
-# Join ready meals with the other features
-df_features = df_quint.merge(df_ready_meals, how='inner', on='LSOA11CD')
+df_quint = dash.make_quintiles(df_features)
 
 # encode travel clusters as integers for data studio plotting
-df_encoded = dash.encode_column(df_features, 'travel_cluster')
+df_encoded = dash.encode_column(df_quint, 'travel_cluster')
 
 # pivot the data set
 df_features_pivot = dash.pivot_results(df_encoded)
 
-### PRETTY NAMES ###
+#### PRETTY NAMES ####
 
 # Rename column to fit into the for loop below
 df_features_pivot.rename({'feature':'Features'}, axis='columns', inplace=True)
@@ -88,10 +69,9 @@ for df in df_resid_list:
     df = dash.pretty_rename(df, 'tc_short_name', cf.tc_short_names)
     
 
-# filter df_features DataFrame for just the ranks and quintile scores
-search_terms = ['Rank','Quintile']
-
-df_features_pivot = df_features_pivot[df_features_pivot['Features'].str.contains("|".join(search_terms))]
+# filter for rows containing quintile scores
+search_term = 'Quintile'
+df_features_pivot = df_features_pivot[df_features_pivot['Features'].str.contains("|".join(search_term))]
 
 
 # Write to BigQuery
